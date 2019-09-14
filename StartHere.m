c1dc130055cdd2841394cb07d8d573664d15f35d
cmrnfaith <43867542+cmrnfaith@users.noreply.github.com>
2019-09-14 12:23:47 -0600

    Map Plot attempt

---
+++ StartHere.m
@@ -0,0 +1,6 @@
+uiimport('-file');
+Lat = nonzeros(LatitudeDegrees1800180050');
+Long = nonzeros(LongitudeDegrees1800180050');
+scatter(Lat, Long);
+xlim([40.8429 40.8471]);
+ylim([-97.7696 -97.76691]);
--- TrackAppScriptFile.m
+++ TrackAppScriptFile.m
@@ -1,20 +1,26 @@
-CSVdata = importdata
-
 classdef TrackAppScriptFile < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure            matlab.ui.Figure
-        CSVLineSliderLabel  matlab.ui.control.Label
-        CSVLineSlider       matlab.ui.control.Slider
-        TabGroup            matlab.ui.container.TabGroup
-        TestGraphsTab       matlab.ui.container.Tab
-        UIAxes              matlab.ui.control.UIAxes
-        EngineTab           matlab.ui.container.Tab
-        GaugeLabel          matlab.ui.control.Label
-        Gauge               matlab.ui.control.Gauge
+        UIFigure             matlab.ui.Figure
+        TabGroup             matlab.ui.container.TabGroup
+        TestGraphsTab        matlab.ui.container.Tab
+        UIAxes               matlab.ui.control.UIAxes
+        EngineTab            matlab.ui.container.Tab
+        GaugeLabel           matlab.ui.control.Label
+        Gauge                matlab.ui.control.Gauge
+        CSVLINESpinnerLabel  matlab.ui.control.Label
+        CSVLINESpinner       matlab.ui.control.Spinner
     end
 
+
+    properties (Access = private)
+    end
+
+    methods (Access = private)
+    
+        
+    end
 
 
     methods (Access = private)
@@ -22,6 +28,14 @@ classdef TrackAppScriptFile < matlab.apps.AppBase
         % Size changed function: TestGraphsTab
         function TestGraphsTabSizeChanged(app, event)
 
+        end
+
+        % Value changing function: CSVLINESpinner
+        function CSVLINESpinnerValueChanging(app, event)
+            changingValue = event.Value;
+            val = app.CSVLINESpinner.Value;
+            app.Guage.Value = changingValue;
+            CSVLine = changingValue;
         end
     end
 
@@ -36,19 +50,9 @@ classdef TrackAppScriptFile < matlab.apps.AppBase
             app.UIFigure.Position = [100 100 796 588];
             app.UIFigure.Name = 'UI Figure';
 
-            % Create CSVLineSliderLabel
-            app.CSVLineSliderLabel = uilabel(app.UIFigure);
-            app.CSVLineSliderLabel.HorizontalAlignment = 'right';
-            app.CSVLineSliderLabel.Position = [1 22 56 22];
-            app.CSVLineSliderLabel.Text = 'CSV Line';
-
-            % Create CSVLineSlider
-            app.CSVLineSlider = uislider(app.UIFigure);
-            app.CSVLineSlider.Position = [78 31 236 3];
-
             % Create TabGroup
             app.TabGroup = uitabgroup(app.UIFigure);
-            app.TabGroup.Position = [325 1 472 580];
+            app.TabGroup.Position = [325 1 472 588];
 
             % Create TestGraphsTab
             app.TestGraphsTab = uitab(app.TabGroup);
@@ -63,23 +67,36 @@ classdef TrackAppScriptFile < matlab.apps.AppBase
             app.UIAxes.Box = 'on';
             app.UIAxes.XGrid = 'on';
             app.UIAxes.YGrid = 'on';
-            app.UIAxes.Position = [1 360 300 185];
+            app.UIAxes.Position = [1 368 300 185];
 
             % Create EngineTab
             app.EngineTab = uitab(app.TabGroup);
             app.EngineTab.Title = 'Engine';
+            app.EngineTab.BackgroundColor = [0.502 0.502 0.502];
 
             % Create GaugeLabel
             app.GaugeLabel = uilabel(app.EngineTab);
             app.GaugeLabel.HorizontalAlignment = 'center';
-            app.GaugeLabel.Position = [163 186 42 22];
+            app.GaugeLabel.Position = [163 194 42 22];
             app.GaugeLabel.Text = 'Gauge';
 
             % Create Gauge
             app.Gauge = uigauge(app.EngineTab, 'circular');
             app.Gauge.Limits = [0 14000];
             app.Gauge.MajorTickLabels = {'0', '1400', '2800', '4200', '5600', '7000', '8400', '9800', '11200', '12600', '14000'};
-            app.Gauge.Position = [30 223 308 308];
+            app.Gauge.FontColor = [1 0 0];
+            app.Gauge.Position = [30 231 308 308];
+
+            % Create CSVLINESpinnerLabel
+            app.CSVLINESpinnerLabel = uilabel(app.UIFigure);
+            app.CSVLINESpinnerLabel.HorizontalAlignment = 'right';
+            app.CSVLINESpinnerLabel.Position = [21 2 60 22];
+            app.CSVLINESpinnerLabel.Text = 'CSV LINE';
+
+            % Create CSVLINESpinner
+            app.CSVLINESpinner = uispinner(app.UIFigure);
+            app.CSVLINESpinner.ValueChangingFcn = createCallbackFcn(app, @CSVLINESpinnerValueChanging, true);
+            app.CSVLINESpinner.Position = [96 2 100 22];
         end
     end
 
@@ -87,7 +104,8 @@ classdef TrackAppScriptFile < matlab.apps.AppBase
 
         % Construct app
         function app = TrackAppScriptFile
-
+            %Import data to be plotted
+            CSVdata = uiimport('-file');
             % Create and configure components
             createComponents(app)
 
