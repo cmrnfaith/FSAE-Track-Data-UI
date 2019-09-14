f3eed9c409fa6aa0bc3e3f61ed9971fdcb7afb0a
cmrnfaith <43867542+cmrnfaith@users.noreply.github.com>
2019-09-14 15:06:35 -0600

    Sept 14 Final

--- StartHere.m
+++ StartHere.m
@@ -1,6 +1,8 @@
-uiimport('-file');
-Lat = nonzeros(LatitudeDegrees1800180050');
-Long = nonzeros(LongitudeDegrees1800180050');
-scatter(Lat, Long);
-xlim([40.8429 40.8471]);
-ylim([-97.7696 -97.76691]);
+% uiimport('-file');
+data = uiimport('-file');
+            waitfor(data);
+            Lat = nonzeros(LatitudeDegrees1800180050');
+            Long = nonzeros(LongitudeDegrees1800180050');
+            Speed = nonzeros(SpeedMPH00150050');
+%xlim([40.8429 40.8471]);
+%ylim([-97.7696 -97.76691]);
